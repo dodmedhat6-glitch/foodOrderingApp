7386bcfb25b934e17fbb27b10d7e5d6043cc5bc7
AbdAlrhman_medhat <dodmedhat6@gmail.com>
2026-08-30 13:51:28 +0300

    update validate body function

--- core-service/src/common/validation/validate.ts
+++ core-service/src/common/validation/validate.ts
@@ -1,12 +1,13 @@
 import { validate } from "class-validator";
 import { AppError } from "../error/AppError"
+import {plainToInstance} from "class-transformer";
 
 export async function validateBody<T extends object>(cls: new (...args: any[]) => T, body: unknown): Promise<T> {
     if (typeof cls !== "function") {
         throw new AppError("Invalid validation schema", 500);
     }
 
-    const instance = Object.assign(Object.create(cls.prototype), body);
+    const instance = plainToInstance(cls, body);
     const error = await validate(instance, { whitelist: true });
 
     if (error.length > 0) {
